a50aa5737c0e94e6f4836d2a08861945e979684f
Asatidz <asatidzal.hakiim3@smk.belajar.id>
2025-02-16 23:37:50 +0700

    Update tebus_insert.php

--- content/tebus_insert.php
+++ content/tebus_insert.php
@@ -8,7 +8,7 @@ $kode     = $_POST['tebus'];
 $nis      = $_POST['nis'];
   // Ambil kode tebus dari form
 // Periksa apakah kode tebus ada dalam database
-var_dump($_POST);   
+// var_dump($_POST);   
 // Mengambil poin dari tabel tebus berdasarkan kode
 $query = "SELECT poin_tebus FROM tebus WHERE kode_tebus = '$kode'";
 $result = mysqli_query($con, $query);
@@ -19,7 +19,7 @@ $poin = $data['poin_tebus'];  // Ambil poin dari tebus
 $query = "INSERT INTO rw_tebus (nis, nama, tanggal, kode, poin) VALUES ('$nis','$nama', '$tgl', '$poin', '$kode')";
 $result = mysqli_query($con, $query);
 
-var_dump($poin, $data);
+// var_dump($poin, $data);
 
 
 // Update poin siswa
